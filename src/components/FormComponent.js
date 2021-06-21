522cb7a822c134aab45fe4db798729a95f41f5e0
ashishrainagit2 <ashishraina55@gmail.com>
2021-06-21 16:44:49 +0530

    First Comment

---
+++ src/components/FormComponent.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useFormik } from 'formik'
+
+const FormComponent = () => {
+
+	const formik = useFormik({
+		initialValues : {
+			firstname : '',
+			lastname : '',
+			password: ''
+		}
+	})
+
+	console.log('1 =>' , formik.values)
+    return (
+		<form>
+			<div className="formfield">
+				<label htmlFor="firstname">Name</label>
+				<input type="text" name='firstname' onChange={formik.handleChange} value={formik.values.firstname} />
+			</div>
+
+			<div className="formfield">
+				<label htmlFor="lastname">Lastname</label>
+				<input type="text" name='lastname' onChange={formik.handleChange} value={formik.values.lastname} />
+			</div>
+
+			<div className="formfield">
+				<label htmlFor="password">Password</label>
+				<input type="password" name='password' onChange={formik.handleChange} value={formik.values.password} />
+			</div>
+
+			<button type='submit'>Submit</button> 
+		</form>
+	)
+}
+
+export default FormComponent
+
